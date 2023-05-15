c581412c71febdcaf3c4f93b9036eaf9029a8fd3
Paula Gonzalez <pagoba94@gmail.com>
2023-05-15 18:03:26 +0200

    final commit

---
+++ small-api/tools/sql_queries.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import config.sql_connection as conn
+
+def get_everything ():
+    query = "SELECT * FROM salaries LIMIT 10;"
+    engine = conn.connection_to_sql()
+    df = pd.read_sql_query(query, engine)
+    return df.to_dict(orient="records")
+
+
+def table_ten (tablename):
+    query = f"SELECT * FROM  {tablename} LIMIT 10;"
+    engine = conn.connection_to_sql()
+    df = pd.read_sql_query(query, engine)
+    return df.to_dict(orient="records")
+
+
+
