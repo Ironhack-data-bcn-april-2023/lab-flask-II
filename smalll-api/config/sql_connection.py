9e9baed98cbff8ad33825161eeaf79c4423f5944
Ramses Mendoza <ramsesmr@Ramsess-MacBook-Pro.local>
2023-05-15 17:45:41 +0200

    lab flask 2 Ramses

---
+++ smalll-api/config/sql_connection.py
@@ -0,0 +1,11 @@
+import sqlalchemy as alch
+import os 
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
+password = os.getenv("passwordd")
+
+dbName = "employees"
+connectionData=f"mysql+pymysql://root:{password}@localhost/{dbName}"
+engine = alch.create_engine(connectionData)
