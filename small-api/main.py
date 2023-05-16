460020891f1b83bcca504598fd7a09102786bb9b
LeticiaIruela <leticia.miruela@gmail.com>
2023-05-16 17:28:12 +0200

    up

--- small-api/main.py
+++ small-api/main.py
@@ -23,5 +23,19 @@ def employees_full():
 def one_table (tablename):
     return queries.table_ten(tablename)
 
+@app.route("/insert-into-employees", methods=["POST"])
+def inserts_into_db ():
+    
+    dict_ = {
+    "emp_no": request.args["emp_no"],
+    "birth_date": request.args["birth_date"],
+    "first_name": request.args["first_name"],
+    "last_name": request.args["last_name"],
+    "gender": request.args["gender"],
+    "hire_date": request.args["hire_date"]
+    } 
+
+    return queries.insert_into_employees(dict_)
+
 if __name__ == "__main__":
      app.run(port=9000, debug=False)
